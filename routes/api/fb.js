a5cdf06a5e8ce05558ccc38c5412c1899613ab79
Hardik Menger <hardik.menger@gmail.com>
2018-07-24 19:25:56 +0530

    long lived token

--- routes/api/fb.js
+++ routes/api/fb.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
 //constants
 const secret = require("../../config/keys").secret;
 const appid = require("../../config/keys").appid;
 
 router.get("/auth", function(req, res) {
-  var redirect_uri = "http://localhost:3000/fb/pages";
-  // For eg. "http://localhost:3000/facebook/callback"
+  fetch(
+    `https://graph.facebook.com/oauth/access_token?client_id=${appid}&client_secret=${secret}&grant_type=fb_exchange_token&fb_exchange_token=${userAccessToken}`
+  ).then(response =>
+    response.json().then(res => console.log(res.data.access_token))
+  );
 });
 router.get("/pages", (req, res) => {});
 module.exports = router;
