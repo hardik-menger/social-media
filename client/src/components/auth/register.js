e8db588262a165806d5c716167cdcb402a86b4d1
Hardik Menger <hardik.menger@gmail.com>
2018-08-18 18:49:52 +0530

    fixed the signup form

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -109,9 +109,7 @@ class Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">
-                Create your DevConnector account
-              </p>
+              <p className="lead text-center">Create your account</p>
               <form onSubmit={this.handleSubmit.bind(this)}>
                 <TextInput
                   value={this.state.name}
@@ -128,8 +126,6 @@ class Register extends Component {
                   error={errors.email}
                   placeholder="Email Address"
                   name="email"
-                  info="This site uses Gravatar so if you want a profile image, use
-                  a Gravatar email"
                 />
                 <TextInput
                   value={this.state.password}
