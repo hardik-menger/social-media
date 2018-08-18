2bd00d502711aaf1b1f78627cd64bc396607ac5f
Hardik Menger <hardik.menger@gmail.com>
2018-08-18 23:15:28 +0530

    fixed font

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -121,7 +121,7 @@ class Navbar extends Component {
     );
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
+        <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             Social
           </Link>
